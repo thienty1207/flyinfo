4e994c2aa1cf2136d92831cd85bdca86be614ed0
thienty1207 <tytybill123@gmail.com>
2025-04-20 23:28:51 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,18 +29,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-
+    implementation("net.skyscanner.backpack:backpack-android:43.0.0")
+    
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "flyinfo"
 include(":app")
+ 
